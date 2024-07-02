7d1ea316bf3e113931963981f26e84076a477cce
Ayyan Ahmed <ayunahmed69@gmail.com>
2024-07-02 22:43:40 +0500

    Models Added

--- Cafe/Cafe/settings.py
+++ Cafe/Cafe/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
         'PASSWORD': 'Unhealthynerd69',
         'PORT': '3306',
         'HOST': 'localhost',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- Cafe/restaurant/models.py
+++ Cafe/restaurant/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    ID = models.IntegerField(
+        primary_key=True,
+        validators=[MaxValueValidator(99999999999), MinValueValidator(1)],
+        null=False,
+        blank=False
+    )
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(
+        validators=[MaxValueValidator(999999), MinValueValidator(1)]
+    )
+    BookingDate = models.DateTimeField()
+
+class Menu(models.Model):
+    ID = models.IntegerField(
+        primary_key=True,
+        validators=[MaxValueValidator(99999), MinValueValidator(1)],
+        null=False,
+        blank=False
+    )
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField(
+        validators=[MaxValueValidator(99999), MinValueValidator(0)]
+    )
