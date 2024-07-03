24e06b54189675f63a4da97793a626b472bb9d37
Ayyan Ahmed <ayunahmed69@gmail.com>
2024-07-03 17:06:24 +0500

    URLs, ViewSets and Generics Views completed

--- Cafe/restaurant/urls.py
+++ Cafe/restaurant/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from . import views
+from django.urls import path, include;
+from . import views;
+from rest_framework import routers;
+
+router = routers.DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('booking/', include(router.urls)),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
